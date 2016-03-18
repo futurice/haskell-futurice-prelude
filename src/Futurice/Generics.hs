8c7cd157507d0fd2c22044f9c153e52e1fdc0f10
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-03-18 09:24:19 +0200

    Add Futurice.Generics

---
+++ src/Futurice/Generics.hs
@@ -0,0 +1,226 @@
+{-# LANGUAGE DataKinds, FlexibleContexts, TypeFamilies, ScopedTypeVariables, RankNTypes #-}
+-- | "Generics.SOP" derivation for record types (i.e. products).
+module Futurice.Generics (
+    -- * QuickCheck
+    sopArbitrary,
+    sopShrink,
+    -- * Cassava
+    sopToNamedRecord,
+    sopHeaderOrder,
+    -- * Aeson
+    sopToJSON,
+    -- * Swagger
+    sopDeclareNamedSchema,
+    ) where
+
+import Futurice.Prelude hiding (Generic, from)
+import Prelude          ()
+
+import Control.Lens      (Lens', lens)
+import Data.ByteString   (ByteString)
+import Data.Char         (toLower)
+import Generics.SOP
+import Generics.SOP.Lens
+
+import qualified Data.Aeson           as Aeson
+import qualified Data.Aeson.Types     as Aeson
+import qualified Data.Csv             as Csv
+import qualified Data.Swagger         as Swagger
+import qualified Data.Swagger.Declare as Swagger
+import qualified Data.Vector          as V
+import qualified Test.QuickCheck      as QC
+import qualified GHC.Exts             as Exts
+
+-------------------------------------------------------------------------------
+-- QuickCheck
+-------------------------------------------------------------------------------
+
+sopArbitrary
+    :: (Generic a, All QC.Arbitrary xs, Code a ~ '[xs])
+    => QC.Gen a
+sopArbitrary = to . SOP . Z <$> sopArbitrary'
+
+sopArbitrary' :: All QC.Arbitrary xs => QC.Gen (NP I xs)
+sopArbitrary' = hsequence $ hcpure (Proxy :: Proxy QC.Arbitrary) QC.arbitrary
+
+-- | Works for types isomorphic to some tuple.
+sopShrink
+    :: forall a xs. (Generic a, All QC.Arbitrary xs, Code a ~ '[xs])
+    => a -> [a]
+sopShrink = map (to . SOP . Z) . sopShrink' . unSOPZ . from
+  where
+    unSOPZ :: SOP I '[xs] -> NP I xs
+    unSOPZ (SOP (Z xs)) = xs
+    unSOPZ _            = error "unSOPZ: impossible happened!"
+
+    sopShrink' :: forall ys. All QC.Arbitrary ys => NP I ys -> [NP I ys]
+    sopShrink' Nil = []
+    sopShrink' (I x :* Nil) = map (^. isoI . singletonP) $ QC.shrink x 
+    sopShrink' (I x :* xs)  =
+        [ I x' :* xs  | x'  <- QC.shrink x ] ++
+        [ I x  :* xs' | xs' <- sopShrink' xs ]
+
+-------------------------------------------------------------------------------
+-- Cassava
+-------------------------------------------------------------------------------
+
+sopToNamedRecord
+    :: forall a xs.
+       (Generic a, HasDatatypeInfo a, All Csv.ToField xs, Code a ~ '[xs])
+    => a
+    -> Csv.NamedRecord
+sopToNamedRecord
+    = Csv.namedRecord
+    . sopToNamedRecord' fieldInfos
+    . (^. unsop . unSingletonS)
+    . from
+  where
+    fieldInfos = datatypeInfo (Proxy :: Proxy a) ^.
+        constructorInfo . unSingletonP . fieldInfo
+
+sopToNamedRecord'
+    :: All Csv.ToField xs => NP FieldInfo xs -> NP I xs
+    -> [(ByteString, ByteString)]
+sopToNamedRecord' fs' xs' = go fs' xs'
+  where
+    prefix :: String
+    prefix = longestFieldInfoPrefix fs'
+
+    go :: All Csv.ToField xs => NP FieldInfo xs -> NP I xs -> [(ByteString, ByteString)]
+    go Nil Nil = []
+    go (FieldInfo f :* fs) (I x :* xs) =
+        Csv.namedField (fromString $ processFieldName prefix f) x : go fs xs
+    go _ _ = error "sopToNamedRecord' go: impossible happened"
+
+sopHeaderOrder
+    :: forall a xs.
+       (Generic a, HasDatatypeInfo a, Code a ~ '[xs])
+    => a  -- ^ Unused, only for compatibility with @cassava@'s 'headerOrder'
+    -> Csv.Header
+sopHeaderOrder _ = V.fromList (sopHeaderOrder' fieldInfos)
+  where
+    fieldInfos = datatypeInfo (Proxy :: Proxy a) ^.
+        constructorInfo . unSingletonP . fieldInfo
+
+sopHeaderOrder' :: SListI xs => NP FieldInfo xs -> [ByteString]
+sopHeaderOrder' fs = hcollapse (hmap f fs)
+  where
+    prefix :: String
+    prefix = longestFieldInfoPrefix fs
+
+    f :: FieldInfo a -> K ByteString a
+    f (FieldInfo n) = K . fromString . processFieldName prefix $ n
+
+-------------------------------------------------------------------------------
+-- Aeson
+-------------------------------------------------------------------------------
+
+sopToJSON
+    :: forall a xs.
+       (Generic a, HasDatatypeInfo a, All Aeson.ToJSON xs, Code a ~ '[xs])
+    => a
+    -> Aeson.Value
+sopToJSON
+    = Aeson.object
+    . sopToJSON' fieldInfos
+    . (^. unsop . unSingletonS)
+    . from
+  where
+    fieldInfos = datatypeInfo (Proxy :: Proxy a) ^.
+        constructorInfo . unSingletonP . fieldInfo
+
+sopToJSON'
+    :: All Aeson.ToJSON xs => NP FieldInfo xs -> NP I xs
+    -> [Aeson.Pair]
+sopToJSON' fs' xs' = go fs' xs'
+  where
+    prefix :: String
+    prefix = longestFieldInfoPrefix fs'
+
+    go :: All Aeson.ToJSON xs => NP FieldInfo xs -> NP I xs -> [Aeson.Pair]
+    go Nil Nil = []
+    go (FieldInfo f :* fs) (I x :* xs) =
+        (fromString $ processFieldName prefix f) Aeson..= x : go fs xs
+    go _ _ = error "sopToNamedRecord' go: impossible happened"
+
+-------------------------------------------------------------------------------
+-- swagger
+-------------------------------------------------------------------------------
+
+type SwaggerM = Swagger.Declare (Swagger.Definitions Swagger.Schema)
+type SwaggerPP = (Text, Swagger.Referenced Swagger.Schema)
+
+sopDeclareNamedSchema
+    :: forall a xs proxy.
+       (Generic a, HasDatatypeInfo a, All Swagger.ToSchema xs, Code a ~ '[xs])
+    => proxy a
+    -> SwaggerM Swagger.NamedSchema
+sopDeclareNamedSchema _ = do
+    props <- hsequenceK (hcmap (Proxy :: Proxy Swagger.ToSchema) prop fieldInfos) :: SwaggerM (NP (K SwaggerPP) xs)
+    pure $ Swagger.NamedSchema (Just $ name ^. packed) $ schema (Exts.fromList . hcollapse $ props)
+  where
+    name = datatypeInfo proxy ^. datatypeName
+
+    schema props = mempty
+      & Swagger.type_ .~ Swagger.SwaggerObject
+      & Swagger.properties .~ props
+      & Swagger.required .~ hcollapse (hmap req fieldInfos)
+
+    prefix :: String
+    prefix = longestFieldInfoPrefix fieldInfos
+
+    req :: forall y. FieldInfo y -> K Text y
+    req (FieldInfo n) = K $ processFieldName prefix n ^. packed
+
+    prop :: forall y. Swagger.ToSchema y => FieldInfo y -> K (SwaggerM SwaggerPP) y
+    prop (FieldInfo n) = K $ (,) n' <$> s
+      where
+        n' = processFieldName prefix n ^. packed
+        s = Swagger.declareSchemaRef (Proxy :: Proxy y)
+
+    fieldInfos :: NP FieldInfo xs
+    fieldInfos = datatypeInfo proxy ^. constructorInfo . unSingletonP . fieldInfo
+
+    proxy :: Proxy a
+    proxy = Proxy
+
+-------------------------------------------------------------------------------
+-- Utilities
+-------------------------------------------------------------------------------
+
+longestCommonPrefix :: Eq a => [a] -> [a] -> [a]
+longestCommonPrefix [] _ = []
+longestCommonPrefix _ [] = []
+longestCommonPrefix (x:xs) (y:ys)
+    | x == y    = x : longestCommonPrefix xs ys
+    | otherwise = []
+
+longestFieldInfoPrefix :: NP FieldInfo xs -> String
+longestFieldInfoPrefix Nil = ""
+longestFieldInfoPrefix (FieldInfo _ :* Nil) = ""
+longestFieldInfoPrefix (FieldInfo a :* FieldInfo b :* Nil) =
+    longestCommonPrefix a b
+longestFieldInfoPrefix (FieldInfo a :* xs) =
+    longestCommonPrefix a (longestFieldInfoPrefix xs)
+
+lowerFirst :: String -> String
+lowerFirst []     = []
+lowerFirst (c:cs) = toLower c : cs
+
+processFieldName :: String -> String -> String
+processFieldName pfx = lowerFirst . drop (length pfx)
+
+-------------------------------------------------------------------------------
+-- generics-sop-lens
+-------------------------------------------------------------------------------
+
+fieldInfo :: Lens' (ConstructorInfo xs) (NP FieldInfo xs)
+fieldInfo = lens g s
+  where
+    g :: ConstructorInfo xs -> NP FieldInfo xs
+    g (Record _ fs) = fs
+    g _             = error "fieldInfo get: only record supported"
+
+    s :: ConstructorInfo xs -> NP FieldInfo xs -> ConstructorInfo xs
+    s (Record n _) fs = Record n fs
+    s _ _             = error "fieldInfo set: only record supported"
