6af404d9484ddcf3ac3f9c8af83efcca9531f8ee
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-08-03 22:03:51 +0300

    Add class Unit1

---
+++ src/Futurice/Constraint/Unit1.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances  #-}
+-- | 'Unit1' has trivial instance! Like 'Data.Proxy.Proxy', but in kind
+-- 'Data.Constraint.Constraint'.
+module Futurice.Constraint.Unit1 where
+
+import Futurice.Constraint.ForallSymbol
+
+-- | Class with instances for all types.
+class Unit1 a
+instance Unit1 a
+instance ForallFSymbol Unit1 b where
+    instFSymbol _ _ _ = Dict
