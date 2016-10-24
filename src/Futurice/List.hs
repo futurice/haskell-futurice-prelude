3433d6f679a3648ef5a1195a9e93609d27a7695f
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-10-24 14:07:55 +0300

    Add Head type-family, haddock Futurice.List

--- src/Futurice/List.hs
+++ src/Futurice/List.hs
@@ -8,19 +8,26 @@
 module Futurice.List (
     Append,
     append,
-    -- appendSList,
     TMap,
     npCompToTMap,
+    Head,
     ) where
 
 import Prelude ()
 import Futurice.Prelude
+import Data.Coerce      (coerce)
 import Generics.SOP     ((:.:) (..))
 
 -------------------------------------------------------------------------------
 -- Append
 -------------------------------------------------------------------------------
 
+-- | '++' of type level lists.
+--
+-- >>> :kind! Append '[Bool, Int] '[String, Double]
+-- Append '[Bool, Int] '[String, Double] :: [*]
+-- = '[Bool, Int, String, Double]
+--
 type family Append (xs :: [k]) (ys :: [k]) :: [k] where
     Append '[]       ys= ys
     Append (x ': xs) ys = x ': Append xs ys
@@ -33,10 +40,35 @@ append (x :* xs) ys = x :* append xs ys
 -- TMap
 -------------------------------------------------------------------------------
 
+-- | 'map' over type level lists.
+--
+-- >>> :kind! TMap Maybe '[Bool, Int]
+-- TMap Maybe '[Bool, Int] :: [*]
+-- = '[Maybe Bool, Maybe Int]
+--
 type family TMap f (xs :: [k]) :: [k] where
     TMap f '[] = '[]
     TMap f (x ': xs) = f x ': TMap f xs
 
+-- | /TODO/ can we have implementation using unsafeCoerce?
 npCompToTMap :: NP (f :.: g) xs -> NP f (TMap g xs)
 npCompToTMap Nil = Nil
-npCompToTMap (Comp fg :* xs) = fg :* npCompToTMap xs
+npCompToTMap (fg :* xs) = coerce fg :* npCompToTMap xs -- use coerce!
+
+-------------------------------------------------------------------------------
+-- Head
+-------------------------------------------------------------------------------
+
+-- | 'head' of type level lists. Partial type family.
+--
+-- >>> :kind! Head '[Int, Bool]
+-- Head '[Int, Bool] :: *
+-- = Int
+--
+-- >>> :kind! Head '[]
+-- Head '[] :: k
+-- = Head '[]
+--
+type family Head (xs :: [k]) where
+    Head (x ': xs) = x
+    -- TODO: add TypeError on GHC 8.0?
