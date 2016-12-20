1fe90ed745b5f182a52ffe1b55e29075143740b3
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-12-20 22:36:33 +0200

    Kind of fix Futurice.Monoid.Average

--- src/Futurice/Monoid.hs
+++ src/Futurice/Monoid.hs
@@ -7,12 +7,12 @@ import Futurice.Prelude
 data Average a = Average { _samples :: !a, getAverage :: !a }
   deriving (Eq, Show)
 
-instance Fractional a => Semigroup (Average a) where
-    Average n x <> Average n' x' = Average m y
+instance (Eq a, Fractional a) => Semigroup (Average a) where
+    Average n x <> Average n' x' = if m == 0 then Average 0 0 else Average m y
       where
         m = n + n'
         y = (n * x + n' * x') / m
 
-instance Fractional a => Monoid (Average a) where
+instance (Eq a, Fractional a) => Monoid (Average a) where
     mempty = Average 0 0
     mappend = (<>)
