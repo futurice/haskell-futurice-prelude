c010c6ab976285dfd7405bb399df4bb245299fbf
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-03-10 17:00:17 +0200

    Add Futurice.Clock with clocked

---
+++ src/Futurice/Clock.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE UndecidableInstances #-}
+module Futurice.Clock (
+    MonadClock (..),
+    clocked,
+    TimeSpec (..),
+    timeSpecToSecondsD,
+    ) where
+
+import Control.Monad.Trans
+import System.Clock
+       (Clock (Monotonic), TimeSpec (..), diffTimeSpec, getTime, toNanoSecs)
+
+-- | Class of monads which carry the notion of the current time.
+class Monad m => MonadClock m where
+    monotonicClock :: m TimeSpec
+
+-- | Base instance for IO.
+instance MonadClock IO where
+    monotonicClock = getTime Monotonic
+
+-- | Generic, overlapping instance.
+instance {-# OVERLAPPING #-}
+    ( MonadClock m
+    , MonadTrans t
+    , Monad (t m)
+    ) => MonadClock (t m)
+  where
+    monotonicClock = lift monotonicClock
+
+clocked :: MonadClock m => m a -> m (TimeSpec, a)
+clocked action = do
+    start <- monotonicClock
+    x <- action
+    end <- monotonicClock
+    return (diffTimeSpec end start, x)
+
+-- | Convert 'TimeSpec' to seconds represented by 'Double'
+timeSpecToSecondsD :: TimeSpec -> Double
+timeSpecToSecondsD = (* 1e-9) . fromInteger . toNanoSecs
