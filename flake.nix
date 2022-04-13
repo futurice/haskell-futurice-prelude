ec6c684081a7806df3b641c81127bc98b501f46d
Jan van Brügge <jan.van.brugge@futurice.com>
2022-04-13 13:11:46 +0200

    Add nix flake
    This allows to fetch prebuilt dependencies from the cache

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "The futurice prelude";
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+    let
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          # This overlay adds our project to pkgs
+          futuricePrelude =
+            final.haskell-nix.project' {
+              src = ./.;
+              projectFileName = "cabal.project";
+              compiler-nix-name = "ghc8107";
+              # This is used by `nix develop .` to open a shell for use with
+              # `cabal`, `hlint` and `haskell-language-server`
+              shell.tools = {
+                cabal = {};
+                hlint = {};
+              };
+              # Non-Haskell shell tools go here
+              shell.buildInputs = with pkgs; [
+                nixpkgs-fmt
+              ];
+            };
+        })
+      ];
+      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+      flake = pkgs.futuricePrelude.flake { };
+    in flake // {
+      # Built by `nix build .`
+      defaultPackage = flake.packages."futurice-prelude:lib:futurice-prelude";
+    });
+}
